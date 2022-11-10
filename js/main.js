30a61aea9e693dd436c901895289d783503f8b42
sameer <sameer678sahu@gmail.com>
2022-11-10 20:57:47 +0530

    Scatter plot done

--- js/main.js
+++ js/main.js
@@ -80,29 +80,26 @@ function update(data) {
   yAxisGroup.transition(t).call(yAxisCall)
 
   // JOIN new data with old elements.
-  const rects = g.selectAll("rect")
+  const rects = g.selectAll("circle")
     .data(data, d => d.month)
 
   // EXIT old elements not present in new data.
   rects.exit()
     .attr("fill", "red")
     .transition(t)
-      .attr("height", 0)
-      .attr("y", y(0))
+      .attr("cy", y(0))
       .remove()
 
   // ENTER new elements present in new data...
-  rects.enter().append("rect")
-    .attr("fill", "grey")
-    .attr("y", y(0))
-    .attr("height", 0)
+  rects.enter().append("circle")
+    .attr("fill", "#042db9")
+    .attr("cy", y(0))
+    .attr("r", 5)
     // AND UPDATE old elements present in new data.
     .merge(rects)
     .transition(t)
-      .attr("x", (d) => x(d.month))
-      .attr("width", x.bandwidth)
-      .attr("y", d => y(d[value]))
-      .attr("height", d => HEIGHT - y(d[value]))
+      .attr("cx", (d) => x(d.month) + (x.bandwidth() / 2))
+      .attr("cy", d => y(d[value]))
 
   const text = flag ? "Profit ($)" : "Revenue ($)"
   yLabel.text(text)
